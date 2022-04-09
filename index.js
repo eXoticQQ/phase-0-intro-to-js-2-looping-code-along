d1bf186f72c4bf77c8e40358f9123b25d67d9874
Vitalii <vetal.shtager@gmail.com>
2022-04-09 18:14:00 -0400

    commit tree 534fa16baa1646a4b5e0131b37a068c46a3783af

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`)
+// }
+
+
+// const gifts = ["teddy bear", "drone", "doll"]
+
+// function wrapGifts( gifts ) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`)
+//         debugger
+//     }
+//     return gifts
+// }
+
+// wrapGifts(gifts)
+
+
+const cards = []
+
+function writeCards(name, event) {
+    for( let i = 0; i <name.length; i++) {
+        cards.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+    }
+    return cards
+}
+
+function countDown( i ) {
+    while (i>=0) {
+        console.log(i--)
+    }
+}
